efc84aff76f82f7e2bd943460c8261db327df4a6
IvankaKuzin <kuziniwanka@gmail.com>
2025-03-27 20:00:42 +0200

    wrote result to csv file

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,7 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields, astuple
 from bs4 import BeautifulSoup, ResultSet
 import requests
+import csv
 
 
 HOME_URL = "https://quotes.toscrape.com/page/{page_number}/"
@@ -11,6 +12,9 @@ class Quote:
     text: str
     author: str
     tags: list[str]
+
+
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
 
 
 def get_quotes_data(quotes: ResultSet) -> list[Quote]:
@@ -37,17 +41,23 @@ def get_quotes_from_pages() -> list[Quote]:
         if page.status_code != 200 or counter > 10:
             return all_quotes_from_page
 
-        print("Scraping page", counter)
         soup = BeautifulSoup(page.content, "html.parser")
         quotes = soup.find_all("div", attrs={"class": "quote"})
         all_quotes_from_page.extend(get_quotes_data(quotes))
         counter += 1
 
 
-def main(output_csv_path: str) -> list | None:
-    pass
+def main(output_csv_path: str) -> None:
+    with open(output_csv_path, "w", encoding="utf-8", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(QUOTE_FIELDS)
+        writer.writerows(
+            [
+                astuple(quote)
+                for quote in get_quotes_from_pages()
+            ]
+        )
 
 
 if __name__ == "__main__":
-    print(main("quotes.csv"))
-    print(len(main("quotes.csv")))
+    main("quotes.csv")
